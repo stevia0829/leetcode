f983832548bfec75d9c1e33faa8f86a57d583ac4
unknown <1044686276@qq.com>
2019-10-29 17:43:49 +0800

    1

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char name[50];
+
+	cout << "please enter your name: ";
+	cin >> name;
+	cout << "your name is: " << name << endl;
+
+}
